542592bd3079161a428b7a301c88b08ec37c77ab
Carlos Enrique Lemus Serrano <cwlemus@gmail.com>
2021-04-08 04:04:43 -0500

    [08-04-2021] Se agrego mensaje de bienvenida

--- NetAppEjemplo/Program.cs
+++ NetAppEjemplo/Program.cs
@@ -7,6 +7,7 @@ namespace NetAppEjemplo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Agregue cambios, Hola Git ");
         }
     }
 }
